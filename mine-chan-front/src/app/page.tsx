32c4a62a62010933b53280a222c2fb473ea48716
soraride <sorarideblog@gmail.com>
2024-03-22 20:27:30 +0900

    add 適当な入力と画像

--- mine-chan-front/src/app/page.tsx
+++ mine-chan-front/src/app/page.tsx
@@ -2,8 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <h1>まいんちゃんをさがせ！</h1>
+
+      <div className="my-10">
+        <Image
+          src="/mine-chan.png"
+          alt="まいんちゃん"
+          width={200}
+          height={200}
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <label htmlFor="num_bomb">爆弾数</label>
+        <input type="number" id="num_bomb" />
+        <button>ひとりプレイ</button>
+      </div>
     </main>
   );
 }
