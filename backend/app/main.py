bba761c68d9746a1b222e44b70137930e66782ae
Soraride <sorarideblog@gmail.com>
2024-03-24 12:58:05 +0900

    tmp

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,34 +1,68 @@
-from fastapi import FastAPI
-import firebase_admin
-from firebase_admin import firestore
+import datetime
 
-from app.schemas import CreateGame
+from fastapi import FastAPI
+from google.cloud import firestore
+
+from app.core import init_field
+from app.schemas import CreateGame, Game, Field
 
 app = FastAPI()
 
-firebase_app = firebase_admin.initialize_app()
-db = firestore.client()
+db = firestore.AsyncClient()
+
+games_collection = db.collection('games')
+fields_collection = db.collection('fields')
+users_collection = db.collection('users')
+config_collection = db.collection("config")
+
+
+test_list = list()
+
+
+@app.get('/')
+async def root():
+    test_list.append(datetime.datetime.now())
+    return test_list
 
 
 @app.get("/hello")
-def get_hello():
-    config_ref = db.collection("config")
-    doc_ref = config_ref.document("hello")
-    doc = doc_ref.get()
+async def get_hello():
+    doc_ref = config_collection.document("hello")
+    doc = await doc_ref.get()
     if doc.exists:
         return doc.to_dict()
     else:
         return {"message": "no data"}
 
 
-
-
 @app.post("/game")
-def create_game(game_data: CreateGame):
+async def create_game(request: CreateGame):
     # ゲームを作成
-    games = db.collection("games")
-    games.add()
+    new_game_ref = games_collection.document()
+    game = Game(
+        game_id=new_game_ref.id,
+        num_players=request.num_player,
+        num_mines=request.num_mines,
+        created_by=firestore.SERVER_TIMESTAMP
+    )
+    await new_game_ref.set(dict(game))
     
     # フィールドを作成（初期化）
-    # 地雷を配置
+    field = Field(
+        game_id=new_game_ref.id,
+        x=request.x,
+        y=request.y,
+        mines=init_field(
+            x=request.x,
+            y=request.y,
+            num_mines=request.num_mines
+        ),
+        flags=list(),
+        opens=list(),
+    )
     
+    # 地雷を配置
+    field_ref = fields_collection.document()
+    await field_ref.set(dict(field))
+    
+    return
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,11 +1,14 @@
-from typing import List, Tuple
+import dataclasses
+from typing import List, Tuple, NamedTuple
 
 from pydantic import BaseModel, PositiveInt
 
 
 class CreateGame(BaseModel):
-    num_player: int
-    num_mines: int
+    num_player: PositiveInt
+    num_mines: PositiveInt
+    x: PositiveInt
+    y: PositiveInt
 
 
 class User(BaseModel):
@@ -13,18 +16,20 @@ class User(BaseModel):
     display_name: str
 
 
+@dataclasses.dataclass
 class Game(BaseModel):
-    id: str
+    game_id: str
     num_players: PositiveInt
     num_mines: PositiveInt
     created_by: str
 
 
-class Vertex(BaseModel):
+class Vertex(NamedTuple):
     x: PositiveInt
     y: PositiveInt
 
 
+@dataclasses.dataclass
 class Field(BaseModel):
     game_id: str
     x: PositiveInt
